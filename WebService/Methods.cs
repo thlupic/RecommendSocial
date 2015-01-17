de0e2220cbb2987f20730bd3b638d01c5ceb5709
thlupic <tomislav.hlupic@gmail.com>
2015-01-17 12:45:12 +0100

    Added support for web services
    Added support for
    JSON
    GET/PUT methods

---
+++ WebService/Methods.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace WebService
+{
+    public class Methods
+    {
+        public string GET(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                }
+                throw;
+            }
+        }
+
+        public void POST(string url, string jsonContent)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            Byte[] byteArray = encoding.GetBytes(jsonContent);
+
+            request.ContentLength = byteArray.Length;
+            request.ContentType = @"application/json";
+
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+            long length = 0;
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    length = response.ContentLength;
+                }
+            }
+            catch (WebException ex)
+            {
+                var e = ex;
+            }
+        }
+
+        public void DELETE(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "DELETE";
+
+            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            Byte[] byteArray = encoding.GetBytes("");
+
+            request.ContentLength = byteArray.Length;
+            request.ContentType = @"application/json";
+
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+            long length = 0;
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    length = response.ContentLength;
+                }
+            }
+            catch (WebException ex)
+            {
+                var e = ex;
+            }
+        }
+    }
+}
---
+++ WebService/Serialization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Net;
+using System.IO;
+using System.Reflection;
+
+namespace WebService
+{
+    public class Serialization
+    {
+        public string Serializer<T>(T objectToSerialize)
+        {
+            var seralizedString = "";
+            var serializer = new JavaScriptSerializer();
+
+            seralizedString = serializer.Serialize(objectToSerialize);
+
+            return seralizedString;
+        }
+
+        public object Deserialize(string stringToDeserialize, Type deserializeObjectType)
+        {
+            var serializer = new JavaScriptSerializer();
+
+            return serializer.Deserialize(stringToDeserialize, deserializeObjectType);
+        }
+    }
+}
